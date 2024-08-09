44a3a4c2fd4c2aa610b560393feff4f011817637
andregalastri <contato@andregalastri.com.br>
2024-08-09 16:49:54 -0300

    Commit inicial

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'andregalastri/galastri-framework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '85ab4637e02d1e867493de1344bf2ee81533c247',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'andregalastri/galastri-framework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '85ab4637e02d1e867493de1344bf2ee81533c247',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

f5954d17e5a2dac3719115e70efcba0c29fc4773
andregalastri <contato@andregalastri.com.br>
2025-01-23 01:05:32 -0300

    Update

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,12 +2,11 @@
 
 return [
     '/' => [
+        'projectName' => 'Galastri Framework',
         'templateFile' => '/app/templates/main.php',
         'output' => 'view',
 
-        '/images' => [
-            'output' => 'file',
-            'baseFolder' => '/app/images',
+        '@main' => [
         ],
     ],
 ];
--- galastri/core/Router.php
+++ galastri/core/Router.php
@@ -9,56 +9,329 @@ namespace galastri\core;
 use galastri\core\config\Config;
 use galastri\extensions\Exception;
 use galastri\language\Message;
+use galastri\modules\Tools;
 
 final class Router
 {
     private const APP_CONFIG_FILE_ROUTES = PROJECT_DIR.'/app/config/routes.php';
 
+    private static array $routeData;
+    private static array $urlParts;
+    private static string $nodeName = '';
+    private static string $controllerName = '';
+    private static array $controllerNamespace = ['app', 'controllers'];
+    private static array $dynamicNodes = [];
+    private static string $methodName = 'main';
+    private static bool $isValidRoute = true;
+    private static bool $removedIndexFromNamespace = false;
+    private static array $parameters = [];
+
     private function __construct() {}
 
     public static function run(): void
     {
+        self::$routeData = Config::importConfig(self::APP_CONFIG_FILE_ROUTES);
+
         self::configureUrlRoot();
+        self::setUrlParts();
+        self::resolveNodes();
+        self::configureNodeProperties();
+        self::resolveEndpoints();
+        self::resolveParameters();
     }
 
     private static function configureUrlRoot(): void
     {
-        $routes = Config::importConfig(self::APP_CONFIG_FILE_ROUTES);
-
         try {
-            Config::set('urlRoot', array_key_first($routes));
+            Config::set('urlRoot', array_key_first(self::$routeData));
         } catch (Exception $e) {
             throw new Exception(
                 Message::get("ROUTER_INVALID_URL_ROOT"),
                 [
                     str_replace(PROJECT_DIR.'/', '', self::APP_CONFIG_FILE_ROUTES),
-                    array_key_first($routes),
+                    array_key_first(self::$routeData),
                 ]
             );
         }
     }
 
-    private static function prepareUrlArray(): void
+    private static function setUrlParts(): void
     {
-        // Parameters::setUrlRoot(key($GLOBALS['GALASTRI_ROUTES']) ?? null);
-        
-        // $requestUri = (new TypeString($_SERVER['REQUEST_URI']))->replaceOnce(ltrim(Parameters::getUrlRoot(), '/'), '')
-        //                                                        ->get();
+        $requestUri = self::removePrefixOnce($_SERVER['REQUEST_URI'], Config::get('urlRoot'));
+        $urlSplit = explode('?', $requestUri);
 
-        // $urlArray = explode('?', $requestUri);
-        // $urlArray = explode('/', $urlArray[0]);
+        $querystring = $urlSplit[1] ?? [];
+        $parts = $urlSplit[0] ?? [];
 
-        // if (empty($urlArray[1])) {
-        //     array_shift($urlArray);
-        // }
+        $urlParts = [''];
 
-        // foreach ($urlArray as &$urlNode) {
-        //     $urlNode = '/' . $urlNode;
-        // }
-        // unset($urlNode);
+        foreach (explode('/', $parts) as $partValue) {
+            if (!is_null($partValue) && $partValue !== '') {
+                $urlParts[] = $partValue;
+            }
+        }
 
-        // self::$urlArray = $urlArray;
+        self::$urlParts = $urlParts;
+    }
 
-        // PerformanceAnalysis::flush(PERFORMANCE_ANALYSIS_LABEL);
+    private static function resolveNodes(): void
+    {
+        $found = false;
+
+        foreach (self::$urlParts as $partValue) {
+
+            if (array_key_exists('/'.$partValue, self::$routeData)) {
+                $found = true;
+
+                self::$routeData = self::$routeData['/'.$partValue];
+                self::$nodeName = '/'.$partValue;
+
+                self::processNodeData($partValue);
+                break;
+            }
+
+            if (!$found) {
+                foreach (self::$routeData as $key => $routeData) {
+                    if ('@'.$partValue == $key) {
+                        $found = true;
+                        break 2;
+                    }
+                }
+            }
+
+            if (!$found) {
+                foreach (self::$routeData as $key => $routeData) {
+                    if (strpos($key, '/?') === 0) {
+                        $found = true;
+
+                        self::$routeData = $routeData;
+                        self::$nodeName = $key;
+
+                        self::$dynamicNodes = [ltrim($key, '/?') => $partValue];
+
+                        self::processNodeData($key);
+                        break 2;
+                    }
+                }
+            }
+        }
+    }
+
+    private static function processNodeData(string $className): void
+    {
+        array_shift(self::$urlParts);
+
+        self::configureRouteProperties();
+        self::resolveNamespace($className);
+        self::resolveNodes();
+    }
+
+    private static function configureRouteProperties(): void
+    {
+        self::setConfigIfExists('timezone');
+        self::setConfigIfExists('offline');
+        self::setConfigIfExists('projectName');
+        self::setConfigIfExists('pageTitle');
+        self::setConfigIfExists('authTag');
+        self::setConfigIfExists('authFailRedirect');
+        self::setConfigIfExists('forceRedirect');
+        self::setConfigIfExists('notFoundRedirect');
+        self::setConfigIfExists('output');
+        self::setConfigIfExists('browserCache');
+        self::setConfigIfExists('offlineMessage');
+        self::setConfigIfExists('authFailMessage');
+        self::setConfigIfExists('permissionFailMessage');
+        self::setConfigIfExists('ignoreMimeType');
+
+
+        try {
+            self::setConfigIfExists('namespace');
+        } catch (Exception $e) {
+            throw new Exception(
+                Message::get("ROUTER_INVALID_NAMESPACE"),
+                [
+                    str_replace(PROJECT_DIR.'/', '', self::APP_CONFIG_FILE_ROUTES),
+                    $e->getData('testedValue'),
+                ]
+            );
+        }
+
+        try {
+            self::setConfigIfExists('baseFolder');
+        } catch (Exception $e) {
+            throw new Exception(
+                Message::get("ROUTER_INVALID_BASE_FOLDER"),
+                [
+                    str_replace(PROJECT_DIR.'/', '', self::APP_CONFIG_FILE_ROUTES),
+                    $e->getData('testedValue'),
+                ]
+            );
+        }
+
+        try {
+            self::setConfigIfExists('templateFile');
+        } catch (Exception $e) {
+            throw new Exception(
+                Message::get("ROUTER_INVALID_TEMPLATE_FILE"),
+                [
+                    str_replace(PROJECT_DIR.'/', '', self::APP_CONFIG_FILE_ROUTES),
+                    $e->getData('testedValue'),
+                ]
+            );
+        }
+    }
+
+    private static function configureNodeProperties(): void
+    {
+        if(self::setConfigIfExists('controller')) {
+            self::$controllerName = self::$routeData['controller'];
+            
+            array_pop(self::$controllerNamespace);
+
+            self::$controllerNamespace[] = self::$routeData['controller'];
+        }
+    }
+
+    private static function configureEndpointProperties(): void
+    {
+        self::setConfigIfExists('downloadable');
+        self::setConfigIfExists('allowedExtensions');
+        self::setConfigIfExists('viewPath');
+        self::setConfigIfExists('httpMethod');
+
+        try {
+            self::setConfigIfExists('parameters');
+        } catch (Exception $e) {
+            throw new Exception(
+                Message::get("ROUTER_INVALID_ENDPOINT_PARAMETERS"),
+                [
+                    str_replace(PROJECT_DIR.'/', '', self::APP_CONFIG_FILE_ROUTES),
+                    $e->getData('testedValue'),
+                ]
+            );
+        }
+    }
+
+    private static function resolveEndpoints(): void
+    {
+        if (Config::get('output') !== 'file' and count(preg_grep('/^@main(:|$)/', array_keys(self::$routeData))) == 0) {
+            throw new Exception(
+                Message::get("ROUTER_MAIN_METHOD_NOT_FOUND"),
+                [
+                    str_replace(PROJECT_DIR.'/', '', self::APP_CONFIG_FILE_ROUTES),
+                    self::$nodeName,
+                ]
+            );
+        }
+
+        $methodName = 'main';
+
+        foreach (self::$routeData as $key => $routeData) {
+            if (strpos($key, '@') === 0) {
+                $endpointMethodName = ltrim($key, '@');
+                $urlPartAsMethod = self::$urlParts[0] ?? 'main';
+
+                if ($urlPartAsMethod == $endpointMethodName) {
+                    array_shift(self::$urlParts);
+                    
+                    $methodName = $endpointMethodName;
+                    break;
+                }
+            }
+        }
+
+        self::$routeData = self::$routeData['@'.$methodName];
+
+        self::$methodName = $methodName;
+        self::configureEndpointProperties();
+    }
+
+    private static function resolveParameters(): void
+    {
+        $parametersList = array_values(array_filter(explode('/', Config::get('parameters'))));
+
+        if (count(self::$urlParts) > 0 and count($parametersList) <= 0) {
+            self::$isValidRoute = false;
+        }
+
+        foreach ($parametersList as $parameterId) {
+            if (strpos($parameterId, '?') !== 0 and count(self::$urlParts) <= 0) {
+                self::$isValidRoute = false;
+            }
+
+            self::$parameters[ltrim($parameterId, '?')] = array_shift(self::$urlParts);
+        }
+    }
+
+    private static function resolveNamespace(string $partValue): void
+    {
+        if (isset(self::$routeData['namespace'])) {
+            self::$controllerNamespace = array_filter(explode('\\', self::$routeData['namespace']));
+        }
+
+        if (!self::$removedIndexFromNamespace and count(self::$controllerNamespace) > 3) {
+            self::$removedIndexFromNamespace = true;
+
+            self::$controllerNamespace = array_filter(self::$controllerNamespace, function($value) {
+                return $value !== 'Index';
+            });
+        }
+
+        self::$controllerName = $partValue === '' ? 'Index' : Tools::toPascalCase($partValue);
+
+        self::$controllerNamespace[] = self::$controllerName;
+    }
+
+    /************************ */
+
+    private static function removePrefixOnce(string $string, string $prefix): string
+    {
+        if (str_starts_with($string, $prefix) && (strlen($string) === strlen($prefix) || $string[strlen($prefix)] === '/')) {
+            return substr($string, strlen($prefix));
+        }
+
+        return $string;
+    }
+
+    private static function setConfigIfExists(string $configName): bool
+    {
+        if (isset(self::$routeData[$configName])) {
+            Config::set($configName, self::$routeData[$configName]);
+            return true;
+        }
+
+        return false;
+    }
+
+    /************************ */
+
+    public static function getControllerName(): string
+    {
+        return self::$controllerName;
+    }
+    
+    public static function getControllerNamespace(): array
+    {
+        return self::$controllerNamespace;
+    }
+    
+    public static function getDynamicNodes(): array
+    {
+        return self::$dynamicNodes;
+    }
+    
+    public static function getMethodName(): string
+    {
+        return self::$methodName;
+    }
+    
+    public static function isInvalidRoute(): bool
+    {
+        return self::$isInvalidRoute;
+    }
+    
+    public static function getParameters(): array
+    {
+        return self::$parameters;
     }
 }
